768752bfde034bc3b9318067901e1f9efc5bbe2c
Smith, Blaine <bsmith257@iwcc.edu>
2018-12-10 18:49:51 -0600

    Projects 3 add review

---
+++ BSmith_Prog3/BSmith_Prog3/MoreyPizzaCustomer.aspx
@@ -0,0 +1,65 @@
+﻿<%--
+ Blaine Smith
+ Prog 3 Due Sunday, November 25th 2018
+ NONE
+ This program will allow the user to enter their name and select toppings to add to a pizza,
+ It will then output the toppings that have been selected.
+ --%>
+<%@ Page Language="C#" AutoEventWireup="true" CodeFile="MoreyPizzaCustomer.aspx.cs" Inherits="MoreyPizzaCustomer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Pizza Parlor Tailor</title>
+<style>
+    #pageTitle{
+        text-align: center;
+        height: 65px;
+        margin-top: 102px;
+        color:white;
+    }
+
+    #wrapper{
+        display: block;
+        text-align: center;
+    }
+
+    #alignment{
+        display: inline-block;
+        text-align: left;
+        padding: 3%;
+        background-color:antiquewhite;
+    }
+    body {
+        background-image: url('https://cdn.hipwallpaper.com/i/3/66/AbfRa6.jpg');
+    }
+    p{
+        color:white;
+        font-size: 25px;
+        text-align: center;
+        margin-top: 100px;
+    }
+</style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="wrapper">
+            
+            <h1 id="pageTitle">Welcome to the Pizza Parlor Tailor</h1>
+            <br />
+            <div id="alignment">
+                Enter your name:  <asp:TextBox ID="Name" runat="server"></asp:TextBox><br /><br />
+                Select three toppings:  <br />
+                <asp:CheckBox ID="Pepperoni" text="Pepperoni" runat="server" /><br />
+                <asp:CheckBox ID="MeatLover" text="Meat lover" runat="server" /><br />
+                <asp:CheckBox ID="Pineapple" text="Pineapple" runat="server" /><br />
+                <asp:CheckBox ID="Italian" text="Italian" runat="server" /><br />
+                <br />
+                <br />
+                <input id="Submit" type="submit" value="Submit Order" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ BSmith_Prog3/BSmith_Prog3/MoreyPizzaCustomer.aspx.cs
@@ -0,0 +1,81 @@
+﻿ //Blaine Smith
+ //Prog 3 Due Sunday, November 25th 2018
+ //NONE
+ //This program will allow the user to enter their name and select toppings to add to a pizza,
+ //It will then output the toppings that have been selected.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MoreyPizzaCustomer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+            form1.Visible = false;
+
+            int NumTops = 0;
+            String FinalOrder = "You have chosen a pizza with the following toppings";
+            StringBuilder OutPut = new StringBuilder();
+
+            if (Name.Text == "" || Name.Text == null)
+            {
+                Response.Write("<p>Please enter your name.</p>");
+            }
+
+            if (Pepperoni.Checked)
+            {
+                if (NumTops < 3)
+                {
+                    OutPut.AppendFormat(", {0} ", Pepperoni.Text);
+                }
+                NumTops++;
+            }
+
+            if (MeatLover.Checked)
+            {
+                if (NumTops < 3)
+                {
+                    OutPut.AppendFormat(", {0} ", MeatLover.Text);
+                }
+                NumTops++;
+            }
+
+            if (Pineapple.Checked)
+            {
+                if (NumTops < 3)
+                {
+                    OutPut.AppendFormat(", {0} ", Pineapple.Text);
+                }
+                NumTops++;
+            }
+
+            if (Italian.Checked)
+            {
+                if (NumTops < 3)
+                {
+                    OutPut.AppendFormat(", {0} ", Italian.Text);
+                }
+                NumTops++;
+            }
+
+            if (NumTops < 3 || NumTops > 3)
+            {
+                Response.Write("<p>You must have three toppings</p>");
+            }
+
+            if (NumTops == 3 && Name.Text != "" && Name.Text != null)
+            {
+                Response.Write(String.Format("<p>{0} {1}<br/> Thank you for your business</p>", FinalOrder, OutPut));
+            }
+
+            NumTops = 0;
+            OutPut.Length = 0;
+        }
+    }
+}
